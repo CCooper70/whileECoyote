a6993d078ced2ade51bde9b404606cd96dea51f6
christy cooper betty <ccooper.prsvr@gmail.com>
2023-12-18 14:17:57 -0600

    finished work

---
+++ main.js
@@ -0,0 +1,25 @@
+function numberDoubler(num) {
+while(num < 100){
+    num *= 2
+}
+return num
+}
+
+function stringRepeater(str) {
+    let str2 = "";
+    while(str2.length < 10){
+    str2 = str + str2;
+    }return str2;
+}
+
+function makeDivisible(x, y) {
+while(x % y !== 0){
+x++;
+}
+return x;
+}
+module.exports = {
+    numberDoubler,
+    stringRepeater,
+    makeDivisible
+}
